0779dac1179f1283dc269e9a24a18b8e7066a6fa
Abhishek Pawar <pawarabhi2004@gmail.com>
2025-02-05 22:56:47 +0530

    chore: add google_fonts and path_provider dependencies in pubspec.yaml

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -8,9 +8,11 @@
 import 'dart:io'; // flutter_ignore: dart_io_import.
 import 'package:flutter_image_compress_common/flutter_image_compress_common.dart';
 import 'package:image_picker_android/image_picker_android.dart';
+import 'package:path_provider_android/path_provider_android.dart';
 import 'package:shared_preferences_android/shared_preferences_android.dart';
 import 'package:flutter_image_compress_common/flutter_image_compress_common.dart';
 import 'package:image_picker_ios/image_picker_ios.dart';
+import 'package:path_provider_foundation/path_provider_foundation.dart';
 import 'package:shared_preferences_foundation/shared_preferences_foundation.dart';
 import 'package:file_selector_linux/file_selector_linux.dart';
 import 'package:image_picker_linux/image_picker_linux.dart';
@@ -19,6 +21,7 @@ import 'package:shared_preferences_linux/shared_preferences_linux.dart';
 import 'package:file_selector_macos/file_selector_macos.dart';
 import 'package:flutter_image_compress_macos/flutter_image_compress_macos.dart';
 import 'package:image_picker_macos/image_picker_macos.dart';
+import 'package:path_provider_foundation/path_provider_foundation.dart';
 import 'package:shared_preferences_foundation/shared_preferences_foundation.dart';
 import 'package:file_selector_windows/file_selector_windows.dart';
 import 'package:image_picker_windows/image_picker_windows.dart';
@@ -50,6 +53,15 @@ class _PluginRegistrant {
       }
 
       try {
+        PathProviderAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         SharedPreferencesAndroid.registerWith();
       } catch (err) {
         print(
@@ -73,6 +85,15 @@ class _PluginRegistrant {
       } catch (err) {
         print(
           '`image_picker_ios` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        PathProviderFoundation.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_foundation` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -147,6 +168,15 @@ class _PluginRegistrant {
       } catch (err) {
         print(
           '`image_picker_macos` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        PathProviderFoundation.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_foundation` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
--- lib/features/add_product/presentation/pages/login_page.dart
+++ lib/features/add_product/presentation/pages/login_page.dart
@@ -1,7 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gehnaorg/features/add_product/data/models/login.dart';
 import 'package:gehnaorg/features/add_product/presentation/bloc/login_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../../../../core/constants/constants.dart';
 
@@ -13,151 +16,247 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 255, 235, 235),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Card(
-              elevation: 5,
-              color: kWhite,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Center(
-                  child: Column(
-                    children: [
-                      Text(
-                        'LOGIN',
-                        style: TextStyle(
-                          letterSpacing: 1,
-                          color: kPrimary,
-                          fontSize: 40,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      // Email TextField with Icon
-                      TextField(
-                        controller: emailController,
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          prefixIcon: const Icon(Icons.email, color: kPrimary),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: const BorderSide(color: kPrimary),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-
-                      // Password TextField with Icon
-                      TextField(
-                        controller: passwordController,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          prefixIcon: const Icon(Icons.lock, color: kPrimary),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: const BorderSide(color: kPrimary),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 24),
-
-                      // BlocConsumer for handling state and navigation
-                      BlocConsumer<LoginBloc, LoginState>(
-                        listener: (context, state) {
-                          if (state is LoginSuccess) {
-                            // Navigate to HomePage
-                            Navigator.pushReplacementNamed(context, '/home');
-                          } else if (state is LoginFailure) {
-                            // Show error as a Snackbar
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text("Invalid user! Please contact the administrator."), 
-                                backgroundColor: Colors.red,
-                              ),
-                            );
-                          }
-                        },
-                        builder: (context, state) {
-                          return ElevatedButton(
-                            onPressed: state is LoginLoading
-                                ? null
-                                : () {
-                                    // Dispatch login event
-                                    context.read<LoginBloc>().add(
-                                          LoginUserEvent(
-                                            email: emailController.text,
-                                            password: passwordController.text,
-                                          ),
-                                        );
-                                  },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: kPrimary,
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 50, vertical: 15),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30),
-                              ),
-                            ),
-                            child: state is LoginLoading
-                                ? const CircularProgressIndicator(
-                                    color: Colors.white,
-                                  )
-                                : const Text(
-                                    'Login',
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                        color: kWhite),
-                                  ),
-                          );
-                        },
-                      ),
-                      const SizedBox(height: 20),
-
-                      // Success/Error message handling
-                      BlocBuilder<LoginBloc, LoginState>(
-                        builder: (context, state) {
-                          if (state is LoginSuccess) {
-                            return const Padding(
-                              padding: EdgeInsets.only(top: 10.0),
-                              child: Text(
-                                'Login Successful!',
-                                style: TextStyle(
-                                  color: Colors.green,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                ),
-                              ),
-                            );
-                          } else if (state is LoginFailure) {
-                            return Padding(
-                              padding: const EdgeInsets.only(top: 10.0),
-                              child: Text(
-                                'Login Again',
-                                style: const TextStyle(
-                                  color: Colors.red,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                ),
-                              ),
-                            );
-                          }
-                          return const SizedBox.shrink();
-                        },
-                      ),
-                    ],
-                  ),
+      body: Stack(
+        children: [
+          ColorFiltered(
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.5),
+              BlendMode.darken,
+            ),
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/loginbackground.png'),
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FlipAnimation(),
+                    Card(
+                      elevation: 5,
+                      color: kWhite,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              ShaderMask(
+                                shaderCallback: (bounds) => LinearGradient(
+                                  colors: [
+                                    const Color.fromARGB(255, 0, 0, 0),
+                                    kPrimary
+                                  ],
+                                ).createShader(bounds),
+                                child: Text(
+                                  'LOGIN',
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: 2,
+                                    color: Colors.white,
+                                    decoration:   TextDecoration.underline,
+                                    decorationColor: kPrimary
+                                  ),
+                                ),
+                              ),
+
+                              const SizedBox(
+                                height: 40,
+                              ),
+                              // Email TextField with Icon
+                              TextField(
+                                controller: emailController,
+                                decoration: InputDecoration(
+                                  labelText: 'Email',
+                                  prefixIcon:
+                                      const Icon(Icons.email, color: kPrimary),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide:
+                                        const BorderSide(color: kPrimary),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 20),
+
+                              // Password TextField with Icon
+                              TextField(
+                                controller: passwordController,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  labelText: 'Password',
+                                  prefixIcon:
+                                      const Icon(Icons.lock, color: kPrimary),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide:
+                                        const BorderSide(color: kPrimary),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 24),
+
+                              // BlocConsumer for handling state and navigation
+                              BlocConsumer<LoginBloc, LoginState>(
+                                listener: (context, state) {
+                                  if (state is LoginSuccess) {
+                                    // Navigate to HomePage
+                                    Navigator.pushReplacementNamed(
+                                        context, '/home');
+                                  } else if (state is LoginFailure) {
+                                    // Show error as a Snackbar
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text(
+                                            "Invalid user! Please contact the administrator."),
+                                        backgroundColor: Colors.red,
+                                      ),
+                                    );
+                                  }
+                                },
+                                builder: (context, state) {
+                                  return ElevatedButton(
+                                    onPressed: state is LoginLoading
+                                        ? null
+                                        : () {
+                                            // Dispatch login event
+                                            context.read<LoginBloc>().add(
+                                                  LoginUserEvent(
+                                                    email: emailController.text,
+                                                    password:
+                                                        passwordController.text,
+                                                  ),
+                                                );
+                                          },
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: kPrimary,
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 50, vertical: 15),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                      ),
+                                    ),
+                                    child: state is LoginLoading
+                                        ? const CircularProgressIndicator(
+                                            color: Colors.white,
+                                          )
+                                        : const Text(
+                                            'Login',
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold,
+                                                color: kWhite),
+                                          ),
+                                  );
+                                },
+                              ),
+                              const SizedBox(height: 20),
+
+                              // Success/Error message handling
+                              BlocBuilder<LoginBloc, LoginState>(
+                                builder: (context, state) {
+                                  if (state is LoginSuccess) {
+                                    return const Padding(
+                                      padding: EdgeInsets.only(top: 10.0),
+                                      child: Text(
+                                        'Login Successful!',
+                                        style: TextStyle(
+                                          color: Colors.green,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    );
+                                  } else if (state is LoginFailure) {
+                                    return Padding(
+                                      padding: const EdgeInsets.only(top: 10.0),
+                                      child: Text(
+                                        'Login Again',
+                                        style: const TextStyle(
+                                          color: Colors.red,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                  return const SizedBox.shrink();
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class FlipAnimation extends StatefulWidget {
+  @override
+  _FlipAnimationState createState() => _FlipAnimationState();
+}
+
+class _FlipAnimationState extends State<FlipAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    )..repeat(reverse: false); // Continuous flipping
+
+    _animation = Tween<double>(begin: 0, end: pi).animate(_controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        double angle = _animation.value;
+        bool isFront = angle < pi / 2; // Check which side is visible
+
+        return Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.identity()
+            ..setEntry(3, 2, 0.001) // Perspective effect
+            ..rotateY(angle),
+          child: isFront
+              ? Image.asset('assets/images/applogo.png',
+                  height: 300) // Front side
+              : Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()..rotateY(pi), // Back side
+                  child: Image.asset('assets/images/applogo.png', height: 300),
+                ),
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
